64f9eaee29dbc69ee2a3099b387f53d7fe50a78d
Fabian Muñoz <luiisfabian96@gmail.com>
2022-02-26 21:28:27 -0500

    creacion y arreglo del formulario del login

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_flutter_app/src/utils/my_colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,26 +12,118 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("login"),
-        ),
-        body: Container(
-          margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 30.0),
-          child: Column(
+      body: Container(
+        child: Container(
+          width: double.infinity,
+          child: ListView(
             children: [
-              Text("PRIMER TEXTO"),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Descripción"),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Ingresar"),
+              Column(
+                children: [
+                  imageBanner(),
+                  textFieldEmail(),
+                  textFieldPassword(),
+                  buttonLogin(),
+                  rowDonotRememberAccount(),
+                ],
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Widget imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.01, bottom: MediaQuery.of(context).size.height * 0.1),
+      child: Image.asset(
+        'assets/img/delivery2.png',
+        
+        width: 200,
+        height: 200,
+      ),
+    );
+  }
+
+  Widget textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      decoration: BoxDecoration(
+        color: myColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: "Email o Usuario",
+          hintStyle: TextStyle(color: myColors.primaryColorDark),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.email,
+            color: myColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textFieldPassword() {
+     return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      decoration: BoxDecoration(
+        color: myColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: "Password",
+          hintStyle: TextStyle(color: myColors.primaryColorDark),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.lock,
+            color: myColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: myColors.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 15),
+        ),
+        onPressed: () {},
+        child: Text("Ingresar"),
+      ),
+    );
+  }
+
+  Widget rowDonotRememberAccount() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "¿No tienes Cuenta?",
+          style: TextStyle(color: myColors.primaryColor),
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Text(
+          "Registrate",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: myColors.primaryColor),
+        )
+      ],
+    );
   }
 }
--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -1,6 +1,8 @@
-
 import 'package:flutter/material.dart';
 
-class myColors{
-  static Color primaryColor = Color(0xFFF71646); 
+class myColors {
+  static Color primaryColor = Color(0xFFF71646);
+  static Color primaryColorDark = Color(0xFFA7001C);
+
+  static Color primaryOpacityColor = Color.fromRGBO(231, 13, 50, 0.09);
 }
